fafc1efc0648b83ccc57f95f2439fbdc20e5cf99
ZMilaStudio <levent.ua@gmail.com>
2026-09-01 18:45:56 +0300

    feat: track whether analysis compares a previous snapshot

--- packages/follow_core/lib/src/application/follow_analysis_result.dart
+++ packages/follow_core/lib/src/application/follow_analysis_result.dart
@@ -6,8 +6,10 @@ abstract class FollowAnalysisResult {
   const FollowAnalysisResult({
     required this.snapshot,
     required this.analysis,
+    this.comparedToPrevious = false,
   });
 
   final FollowSnapshot snapshot;
   final FollowAnalysis analysis;
+  final bool comparedToPrevious;
 }
