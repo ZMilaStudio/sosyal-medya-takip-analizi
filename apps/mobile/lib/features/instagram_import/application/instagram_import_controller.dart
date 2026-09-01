08859c4de9798bf2d9bb1a649818a84ae7b1cdd1
ZMilaStudio <levent.ua@gmail.com>
2026-09-01 18:33:44 +0300

    feat: refresh recent accounts after Instagram import

--- apps/mobile/lib/features/instagram_import/application/instagram_import_controller.dart
+++ apps/mobile/lib/features/instagram_import/application/instagram_import_controller.dart
@@ -58,6 +58,7 @@ class InstagramImportController
       );
 
       await database.saveSnapshot(result.snapshot);
+      ref.invalidate(recentFollowAccountsProvider);
       state = AsyncData(result);
       return result;
     } catch (error, stackTrace) {
