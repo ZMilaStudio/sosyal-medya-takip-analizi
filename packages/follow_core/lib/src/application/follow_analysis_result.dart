68ebc0043fe74800a7f069d41741b968ba16d113
ZMilaStudio <levent.ua@gmail.com>
2026-09-01 16:41:12 +0300

    refactor: add shared follow analysis result

---
+++ packages/follow_core/lib/src/application/follow_analysis_result.dart
@@ -0,0 +1,13 @@
+import '../models/follow_analysis.dart';
+import '../models/follow_snapshot.dart';
+
+/// Shared presentation-facing result for archive-based follow analysis.
+abstract class FollowAnalysisResult {
+  const FollowAnalysisResult({
+    required this.snapshot,
+    required this.analysis,
+  });
+
+  final FollowSnapshot snapshot;
+  final FollowAnalysis analysis;
+}
