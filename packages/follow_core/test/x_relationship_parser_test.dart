32ec6ebd480b4921331db66275949f9a475ff19c
ZMilaStudio <levent.ua@gmail.com>
2026-09-01 16:48:31 +0300

    test: cover X intent user links

--- packages/follow_core/test/x_relationship_parser_test.dart
+++ packages/follow_core/test/x_relationship_parser_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 void main() {
   const parser = XRelationshipParser();
 
-  test('parses official following.js assignment format', () {
+  test('parses direct profile links from official assignment format', () {
     final users = parser.parseJs('''
 window.YTD.following.part0 = [
   {"following":{"accountId":"101","userLink":"https://twitter.com/Alice"}},
@@ -17,7 +17,23 @@ window.YTD.following.part0 = [
     expect(users.every((user) => user.platform == SocialPlatform.x), isTrue);
   });
 
-  test('parses follower wrapper and direct username fallback', () {
+  test('uses stable account id when archive link has no handle', () {
+    final users = parser.parseJs('''
+window.YTD.follower.part0 = [
+  {"follower":{"accountId":"755137239156490240","userLink":"https://twitter.com/intent/user?user_id=755137239156490240"}}
+]
+''');
+
+    expect(users, hasLength(1));
+    expect(users.single.username, 'id_755137239156490240');
+    expect(users.single.platformUserId, '755137239156490240');
+    expect(
+      users.single.profileUrl.toString(),
+      'https://twitter.com/intent/user?user_id=755137239156490240',
+    );
+  });
+
+  test('parses follower wrapper and explicit username fallback', () {
     final users = parser.parseJs('''
 window.YTD.follower.part0 = [
   {"follower":{"accountId":"303","userLink":"https://mobile.twitter.com/carol/"}},
