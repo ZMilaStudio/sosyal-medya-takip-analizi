a126272c3a09f74d2214f91ab2165b791c3ff8a0
ZMilaStudio <levent.ua@gmail.com>
2026-09-03 23:38:40 +0300

    Initialize consent-aware mobile ads

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
+import 'core/ads/ads_coordinator.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: SosyalMedyaTakipApp()));
+  unawaited(AdsCoordinator.instance.initialize());
 }
